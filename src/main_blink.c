1c7d51efed8100defd926b74abb4c481c291cef2
sidar-yesilyurt <yesilyurtsidar@gmail.com>
2025-08-06 23:01:36 +0100

    empty

--- src/main_blink.c
+++ src/main_blink.c
@@ -1,20 +1,20 @@
 #include "pico/stdlib.h"
 
-#define LED_PIN 25
+#define LED_PIN 25 // Define a constant for the LED pin
 
 int main() {
     // Initialize the LED pin
     gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_set_dir(LED_PIN, GPIO_OUT); // Set the pin for output
 
     while (true) {
         // Turn the LED on
         gpio_put(LED_PIN, 1);
-        sleep_ms(1000); // Wait for 1 second
+        sleep_ms(1000); // Wait for 1 second/1000ms
 
         // Turn the LED off
         gpio_put(LED_PIN, 0);
-        sleep_ms(1000); // Wait for 1 second
+        sleep_ms(1000); // Wait for 1 second/1000ms
     }
 
     return 0;
